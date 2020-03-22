02dc4de81698dc95924a63091a31993f2fe11f3d
deku-M-O <1779881125@qq.com>
2020-03-22 10:33:26 +0800

    Update 作业.py

--- 作业.py
+++ 作业.py
@@ -5,3 +5,12 @@ def os(a,b):
             e.append(i)
     print(a)
 os(1,100)
+
+class Os:
+    def tq(self,a,b):
+        e=[]
+        for i in range(a,b+1):
+            if i%2==0:
+                e.append(i)
+        print(a)
+h=Os
