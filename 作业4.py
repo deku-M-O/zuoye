4e48dbf06ce707f63c4fd206bf68d99ef4cd2ec9
deku-M-O <1779881125@qq.com>
2020-03-30 15:40:20 +0800

    改良

--- 作业4.py
+++ 作业4.py
@@ -2,3 +2,10 @@ print(5*"*")
 for i in range(3):
     print(1*"*"+3*(" ")+1*"*")
 print(5*"*")
+
+def fang(a):
+    print(a*"*")
+    for i in range(a):
+        print(1*"*"+(a-2)*(" ")+1*"*")
+    print(a*"*")
+fang(8)
