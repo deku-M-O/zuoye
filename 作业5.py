d309615ed64ad2bfc080a33196e1c43c83d72c6c
deku-M-O <1779881125@qq.com>
2020-04-12 12:04:39 +0800

    Update 作业5.py

--- 作业5.py
+++ 作业5.py
@@ -1,4 +1,5 @@
 a=float(input("请输入需要转换的数字："))
+yxs=int(input("请输入想要的小数位数："))
 b=[]
 xsl=[]
 e=100
@@ -6,17 +7,19 @@ zs=int(a)
 xs=a-zs
 while e!=0:
     e=zs//2
-    b.append(str(a%2))
+    b.append(str(zs%2))
     zs=e
 q=len(b)-1
 for i in range(q//2):
     b[i],b[q-i]=b[q-i],b[i]
-for h in range(4):
+for h in range(yxs):
     xs=xs*2
     if xs>=1:
         xsl.append(str(1))
         xs=xs-1
     elif xs<1:
         xsl.append(str(0))
+    elif xs==1:
+        break
 print(''.join(b)+"."+''.join(xsl))
 #print(b[::-1])
