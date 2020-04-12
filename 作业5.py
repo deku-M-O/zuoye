8931e09da93bea790551ba94bc1bae19c5e0a94a
deku-M-O <1779881125@qq.com>
2020-04-12 11:43:23 +0800

    Update 作业5.py

--- 作业5.py
+++ 作业5.py
@@ -1,14 +1,21 @@
 a=float(input("请输入需要转换的数字："))
 b=[]
+xsl=[]
 e=100
 zs=int(a)
 xs=a-zs
 while e!=0:
     e=zs//2
-    b.append(a%2)
+    b.append(str(a%2))
     zs=e
 q=len(b)-1
 for i in range(q//2):
     b[i],b[q-i]=b[q-i],b[i]
-print(b)
+for h in range(4):
+    if xs*2>=1:
+        xsl.append(str(1))
+        xs-1
+    elif xs*2<1:
+        xsl.append(str(0))
+print(''.join(b)+"."+''.join(xsl))
 #print(b[::-1])
