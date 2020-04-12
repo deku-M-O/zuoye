59653f4f6962fb865088171fcc83e0caa77b9e6d
deku-M-O <1779881125@qq.com>
2020-04-12 11:46:12 +0800

    Update 作业5.py

--- 作业5.py
+++ 作业5.py
@@ -12,10 +12,11 @@ q=len(b)-1
 for i in range(q//2):
     b[i],b[q-i]=b[q-i],b[i]
 for h in range(4):
-    if xs*2>=1:
+    xs=xs*2
+    if xs>=1:
         xsl.append(str(1))
         xs-1
-    elif xs*2<1:
+    elif xs<1:
         xsl.append(str(0))
 print(''.join(b)+"."+''.join(xsl))
 #print(b[::-1])
