5057dc25281ec80c1867d62716b1a13c7070ff44
deku-M-O <1779881125@qq.com>
2020-05-03 09:58:43 +0800

    Update 二转十.py

--- 二转十.py
+++ 二转十.py
@@ -1,9 +1,15 @@
 a=input("请输入二进制数字：")
 b=0
 a=a.split('.')
-for i,j in enumerate(a[0]):
-    b+=int(j)*2**(len(a[0])-1-i)
-p=0
-for l,h in enumerate(a[1]):
-    p+=int(h)*2**(-1-l)
-print(p+b)
+if int(len(a))==1:
+    for i,j in enumerate(a[0]):
+        b+=int(j)*2**(len(a[0])-1-i)
+elif int(len(a))==2:
+    for i,j in enumerate(a[0]):
+        b+=int(j)*2**(len(a[0])-1-i)
+    p=0
+    for l,h in enumerate(a[1]):
+        p+=int(h)*2**(-1-l)
+    print(p+b)
+else:
+    print("错误")
